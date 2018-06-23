34cec7b64470bb3c6dfca49d45cbd2f200b646b0
Dave Young <dave.andrew.young@gmail.com>
2018-06-23 10:51:24 -0500

    working tabs example now

--- src/App.js
+++ src/App.js
@@ -2,45 +2,28 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import Cam from './Cam';
 import './App.css';
-
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import {Tabs, Tab} from 'material-ui/Tabs';
-import Slider from 'material-ui/Slider';
 
-const styles = {
-  headline: {
-    fontSize: 24,
-    paddingTop: 16,
-    marginBottom: 12,
-    fontWeight: 400,
-  },
-};
-
-function handleActive(tab) {
-  alert(`A tab with this route property ${tab.props['data-route']} was activated.`);
-}
 
 class App extends Component {
   render() {
     return (
+	<MuiThemeProvider>
       <div className="App">
       <Tabs>
         <Tab label="Cam" >
-          <div>
             <Cam />
-          </div>
         </Tab>
         <Tab label="Temperature" >
-          <div>
-            <iframe src="http://192.168.86.71:3000/dashboard-solo/db/monitoring-environment?orgId=1&panelId=1" width="225" height="200" frameborder="0"></iframe>
-          </div>
+            <iframe title='a' src="http://192.168.86.71:3000/dashboard-solo/db/monitoring-environment?orgId=1&panelId=1" width="225" height="200" frameborder="0"></iframe>
         </Tab>
         <Tab label="Humidity" >
-          <div>
-            <iframe src="http://192.168.86.71:3000/dashboard-solo/db/monitoring-environment?orgId=1&panelId=4" width="225" height="200" frameborder="0"></iframe>
-          </div>
+            <iframe title='b' src="http://192.168.86.71:3000/dashboard-solo/db/monitoring-environment?orgId=1&panelId=4" width="225" height="200" frameborder="0"></iframe>
         </Tab>
       </Tabs>
       </div>
+	    </MuiThemeProvider>
     );
   }
 }
